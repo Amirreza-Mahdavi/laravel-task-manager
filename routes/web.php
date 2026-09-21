057d9b16c6811dd028b58725f55be3fa9cd0b813
Amirreza Mahdavi <amiirreza.mahdavii@gmail.com>
2026-09-21 15:29:38 +0330

    feat(routes): add initial routes

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,25 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\AssignmentController;
  
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello', [TaskController::class, 'index']);
+// authentication
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+// task
+Route::post('/tasks', [TaskController::class, 'addTask']);
+Route::post('/taks/{id}/subtasks', [TaskController::class, 'addSubtask']);
+Route::put('/tasks/{id}', [TaskController::class, 'updateTask']);
+Route::delete('tasks/{id}/delete', [TaskController::class, 'deleteTask']);
+
+ // assignment
+Route::post('/tasks/{userId}/assign/{taskId}', [AssignmentController::class, 'assignTask']);
+
