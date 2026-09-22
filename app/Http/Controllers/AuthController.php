ffc224ab7d7c1eddda87e2a8ada610304d8c94a9
Amirreza Mahdavi <amiirreza.mahdavii@gmail.com>
2026-09-22 19:01:28 +0330

    add authentication

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\RegisterRequest;
+use App\Http\Requests\LoginRequest;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Nette\Schema\ValidationException;
+use Illuminate\Http\Controllers\Hash;
+
+class AuthController extends Controller {
+    public function register(RegisterRequest $request): JsonResponse {
+        $user = User::create([
+            'name' => $request->validated('name'),
+            'email' => $request->validated('email'),
+            'password' => $request->validated('password'),
+        ]);
+
+        $token = $user->createToken('api-token')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token,
+        ], 201);
+    }
+
+    public function login(LoginRequest $request): JsonResponse {
+        $data = $request->validated();
+
+        $user = User::where('email', $data['email'])->first();
+
+        if(!$user || !Hash::check($data['$password'], $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect'],
+            ]);
+        }
+        $token = $user->createToken('api-token')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token,
+        ]);
+    }
+
+    public function me(Request $request): JsonResponse {
+        return response()->json([
+            'user' => $request->user(),
+        ]);
+    }
+
+    public function logout(Request $request): JsonResponse {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            'message' => 'Logged out successfully',
+        ]);
+    }
+
+    
+}
