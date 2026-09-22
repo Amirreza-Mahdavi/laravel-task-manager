68f54966db859c3b96399bece00e75fb0831c2b2
Amirreza Mahdavi <amiirreza.mahdavii@gmail.com>
2026-09-22 18:52:55 +0330

    feat(controller): add TaskController

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,10 +2,48 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreateTaskRequest;
 use Illuminate\Http\Request;
+use App\Service\TaskService;
+use App\Http\Requests\UpdateTaskRequest;
+use App\Http\Resources\taskResource;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use App\Models\Task;
+use Illuminate\Http\JsonResponse;
 
 class TaskController extends Controller {
-    public function index(){
-        return view('index');
+    public function __construct(
+        private TaskService $taskService
+    ){}
+    public function index(): AnonymousResourceCollection {
+        $tasks = $this->taskService->getTasks();
+
+        return taskResource::collection($tasks);
+    }
+
+    public function storeTask(CreateTaskRequest $request): taskResource {
+        $task = $this->taskService->createTask($request->user(), $request->validated());
+
+        return new taskResource($task);
+    }
+
+    public function storeSubtask(CreateTaskRequest $request, Task $task): taskResource {
+        $subtask = $this->taskService->createSubtask($request->user(), $task, $request->validated());
+
+        return new taskResource($subtask);
+    }
+
+    public function update(CreateTaskRequest $request, Task $task): taskResource {
+        $task = $this->taskService->updateTask($task, $request->validated());
+
+        return new taskResource($task);
+    }
+
+    public function destroy(Task $task): JsonResponse {
+        $this->taskService->deleteTask($task);
+
+        return response()->json([
+            'message' => 'Successfully deleted',
+        ]);
     }
 }
