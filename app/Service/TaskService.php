e6a6cac18f79e3c1b1730fb7fe0b80091064c93a
Amirreza Mahdavi <amiirreza.mahdavii@gmail.com>
2026-09-22 18:40:13 +0330

    feat(service): add TaskService

---
+++ app/Service/TaskService.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Service;
+
+use App\Models\Task;
+use App\Models\User;
+use RuntimeException;
+
+class TaskService {
+    public function getTasks() {
+        return Task::latest()->get();
+    }
+
+    public function createTask(User $user, array $data): Task {
+        return $user->tasks()->create($data);
+    }
+
+    public function createSubtask(User $user, Task $parentTask, array $data): Task {
+        return $parentTask->childTasks()->create([
+            ...$data,
+            'user_id' => $user->id,
+        ]);
+    }
+
+    public function updateTask(Task $task, array $data): Task {
+        $task->update($data);
+        return $task;
+    }
+
+    public function deleteTask(Task $task): void {
+        if ($task->assignments()->exists())
+            throw new RuntimeException("Task cannot be deleted because it has assignments");
+
+        $task->delete();
+    }
+}
