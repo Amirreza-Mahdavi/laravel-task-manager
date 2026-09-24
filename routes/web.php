2506b15ab57fc41113ee42c68fe50f9fd2495ba5
Amirreza Mahdavi <amiirreza.mahdavii@gmail.com>
2026-09-24 21:44:58 +0330

    add web routes

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,57 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\TaskController;
-use App\Http\Controllers\AssignmentController;
- 
+use App\Http\Controllers\Web\AuthController;
+use App\Http\Controllers\Web\TaskController;
+use App\Http\Controllers\Web\AssignmentController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-// authentication
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
+// Guest routes
+Route::middleware('guest')->group(function () {
 
-// task
-Route::post('/tasks', [TaskController::class, 'addTask']);
-Route::post('/taks/{id}/subtasks', [TaskController::class, 'addSubtask']);
-Route::put('/tasks/{id}', [TaskController::class, 'updateTask']);
-Route::delete('tasks/{id}/delete', [TaskController::class, 'deleteTask']);
+    Route::get('/login', [AuthController::class, 'showLogin'])
+        ->name('login');
 
- // assignment
-Route::post('/tasks/{userId}/assign/{taskId}', [AssignmentController::class, 'assignTask']);
+    Route::post('/login', [AuthController::class, 'login'])
+        ->name('login.store');
 
+    Route::get('/register', [AuthController::class, 'showRegister'])
+        ->name('register');
+
+    Route::post('/register', [AuthController::class, 'register'])
+        ->name('register.store');
+});
+
+
+// Authenticated routes
+Route::middleware('auth')->group(function () {
+
+    Route::post('/logout', [AuthController::class, 'logout'])
+        ->name('logout');
+
+    Route::get('/tasks', [TaskController::class, 'index'])
+        ->name('tasks.index');
+
+    Route::get('/tasks/create', [TaskController::class, 'create'])
+        ->name('tasks.create');
+
+    Route::post('/tasks', [TaskController::class, 'store'])
+        ->name('tasks.store');
+
+    Route::get('/tasks/{task}', [TaskController::class, 'show'])
+        ->name('tasks.show');
+
+    Route::get('/tasks/{task}/edit', [TaskController::class, 'edit'])
+        ->name('tasks.edit');
+
+    Route::put('/tasks/{task}', [TaskController::class, 'update'])
+        ->name('tasks.update');
+
+    Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])
+        ->name('tasks.destroy');
+
+});
