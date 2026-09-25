4877b6e8564a532578933aef810717bac1ca165f
Amirreza Mahdavi <amiirreza.mahdavii@gmail.com>
2026-09-25 20:20:41 +0330

    add auth to layouts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
 
@@ -20,15 +21,95 @@
 
 <body>
 
-    <nav>
-        <a href="{{ route('tasks.index') }}">
+<nav>
+
+    {{-- Logo --}}
+
+    @auth
+
+        @if (auth()->user()->role?->name === 'admin')
+
+            <a href="{{ route('admin.tasks.index') }}">
+                Task Manager
+            </a>
+
+        @else
+
+            <a href="{{ route('tasks.index') }}">
+                Task Manager
+            </a>
+
+        @endif
+
+    @else
+
+        <a href="{{ route('login') }}">
             Task Manager
         </a>
 
-        <div>
-            <a href="{{ route('tasks.index') }}">
-                Tasks
-            </a>
+    @endauth
+
+
+    {{-- Navigation --}}
+
+    <div>
+
+        @auth
+
+            {{-- ADMIN NAVIGATION --}}
+
+            @if (auth()->user()->role?->name === 'admin')
+
+                <a href="{{ route('admin.tasks.index') }}">
+                    Admin Tasks
+                </a>
+
+                <a href="{{ route('admin.tasks.create') }}">
+                    Create Task
+                </a>
+
+            {{-- MEMBER NAVIGATION --}}
+
+            @else
+
+                <a href="{{ route('tasks.index') }}">
+                    My Tasks
+                </a>
+
+                <a href="{{ route('tasks.create') }}">
+                    Create Task
+                </a>
+
+            @endif
+
+
+            {{-- Current user --}}
+
+            <span>
+                Hello, {{ auth()->user()->name }}
+            </span>
+
+
+            {{-- Logout --}}
+
+            <form
+                method="POST"
+                action="{{ route('logout') }}"
+                style="display: inline"
+            >
+
+                @csrf
+
+                <button type="submit">
+                    Logout
+                </button>
+
+            </form>
+
+
+        @else
+
+            {{-- GUEST NAVIGATION --}}
 
             <a href="{{ route('login') }}">
                 Login
@@ -37,12 +118,20 @@
             <a href="{{ route('register') }}">
                 Register
             </a>
-        </div>
-    </nav>
 
-    <main>
-        @yield('content')
-    </main>
+        @endauth
+
+    </div>
+
+</nav>
+
+
+<main>
+
+    @yield('content')
+
+</main>
+
 
 </body>
 </html>
