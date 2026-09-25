b5047d86145f7c83f93ee69d409ac73731150245
Amirreza Mahdavi <amiirreza.mahdavii@gmail.com>
2026-09-25 19:58:47 +0330

    fix validation

--- app/Http/Controllers/Web/AuthController.php
+++ app/Http/Controllers/Web/AuthController.php
@@ -9,68 +9,63 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
 use App\Models\User;
 
-class AuthController extends Controller
-{
-    public function showLogin(): View
-    {
-        dd('LOGIN CONTROLLER WORKS');
+class AuthController extends Controller {
+    public function showLogin(): View {
+        
         return view('auth.login');
     }
 
-    public function showRegister(): View
-    {
-        dd('REGISTER CONTROLLER WORKS');
+    public function showRegister(): View {
+        
         return view('auth.register');
     }
 
-    public function login(Request $request): RedirectResponse
-    {
+    public function login(Request $request): RedirectResponse {
         $credentials = $request->validate([
             'email' => ['required', 'email'],
             'password' => ['required'],
         ]);
 
-        if (Auth::attempt($credentials)) {
-
-            $request->session()->regenerate();
-
-            return redirect()->intended('/tasks');
-        }
-
-        return back()
+        if (!Auth::attempt($credentials)) {
+            return back()
             ->withErrors([
                 'email' => 'The provided credentials are incorrect.',
             ])
             ->onlyInput('email');
+        }
+
+        $request->session()->regenerate();
+        $user = $request->user();
+
+        if ($user->role?->name === 'admin') 
+            return redirect()->route('admin.tasks.index');
+
+        return redirect()->route('tasks.index');
     }
 
-    public function register(Request $request): RedirectResponse
-{
-    $data = $request->validate([
-        'name' => ['required', 'string', 'max:255'],
-        'email' => ['required', 'email', 'max:255', 'unique:users,email'],
-        'password' => ['required', 'string', 'min:8', 'confirmed'],
-    ]);
+    public function register(Request $request): RedirectResponse {
+        $data = $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'email', 'max:255', 'unique:users,email'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ]);
 
-    $user = User::create([
-        'name' => $data['name'],
-        'email' => $data['email'],
-        'password' => $data['password'],
-    ]);
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => $data['password'],
+        ]);
 
-    Auth::login($user);
+        Auth::login($user);
+        $request->session()->regenerate();
 
-    $request->session()->regenerate();
+        return redirect()->intended('/tasks');
+    }
 
-    return redirect()->intended('/tasks');
-}
 
-    public function logout(Request $request): RedirectResponse
-    {
+    public function logout(Request $request): RedirectResponse {
         Auth::logout();
-
         $request->session()->invalidate();
-
         $request->session()->regenerateToken();
 
         return redirect('/login');
